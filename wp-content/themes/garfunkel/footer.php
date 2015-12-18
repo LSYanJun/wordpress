c4edb44e00cc4324f0dfda8c5d94e2007daf39b5
Yanjun Wu <YANJUN@YanjundeMacBook-Pro.local>
2015-12-18 17:42:18 +0800

    wordpress exercise
    yushan website exercise

---
+++ wp-content/themes/garfunkel/footer.php
@@ -0,0 +1,14 @@
+<div class="credits">
+
+	<p>
+	
+		&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>. <?php _e('All rights reserved'); ?>.
+	
+	</p>
+		
+</div> <!-- /credits -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
